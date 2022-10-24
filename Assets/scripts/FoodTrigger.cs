833f8455245a2273b7bf82394d74f2b11a05eaf0
MaxKam <mail@gmail.com>
2022-10-24 20:52:47 +0300

    Make Food eating only

--- Assets/scripts/FoodTrigger.cs
+++ Assets/scripts/FoodTrigger.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class FoodTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject newFood;
+    private Vector3 randomPos;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player"){
+            randomPos = new Vector3(Random.Range(-18f,18f),0.7f,Random.Range(-18f,18f));
+            Instantiate(newFood, randomPos, Quaternion.identity);
             Destroy(this.gameObject);
-            Instantiate(newFood, new Vector3(Random.Range(-18f,18f),0.7f,0), Quaternion.identity);
         }
     }
 }
