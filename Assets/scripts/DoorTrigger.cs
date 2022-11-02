7070cd0e2fdc83600fcb733c802993e81a896269
MaxKam <mail@gmail.com>
2022-11-02 10:09:49 +0300

    alfa 2

--- Assets/scripts/DoorTrigger.cs
+++ Assets/scripts/DoorTrigger.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
+    [SerializeField] private GameObject Door;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            this.gameObject.GetComponent<Animator>().SetTrigger("doOpen");
+            Door.GetComponent<Animator>().SetBool("open", true);
         }
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Door.GetComponent<Animator>().SetBool("open", true);
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            this.gameObject.GetComponent<Animator>().SetTrigger("doClose");
+            Door.GetComponent<Animator>().SetBool("open", false);
         }
     }
 }
